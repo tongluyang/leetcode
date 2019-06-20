4a164c9f72e5b43e684e7e1a2134c3b09f97fced
tongluyang <tongluyang@gmail.com>
2019-06-20 20:20:07 +0800

    有效的括号

---
+++ 20.有效的括号.java
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=java
+ *
+ * [20] 有效的括号
+ *
+ * https://leetcode-cn.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (38.40%)
+ * Likes:    857
+ * Dislikes: 0
+ * Total Accepted:    84.1K
+ * Total Submissions: 218.4K
+ * Testcase Example:  '"()"'
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 
+ * 有效字符串需满足：
+ * 
+ * 
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 
+ * 
+ * 注意空字符串可被认为是有效字符串。
+ * 
+ * 示例 1:
+ * 
+ * 输入: "()"
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: "()[]{}"
+ * 输出: true
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: "(]"
+ * 输出: false
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 输入: "([)]"
+ * 输出: false
+ * 
+ * 
+ * 示例 5:
+ * 
+ * 输入: "{[]}"
+ * 输出: true
+ * 
+ */
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == 40 || c == 91 || c == 123) {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                Character pop = stack.pop();
+
+                if (Math.abs(c - pop) > 2) {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
+
