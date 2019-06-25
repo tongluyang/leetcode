0444b3ce7efe7dd193849a62d5df3697bb5f880b
tongluyang <tongluyang@gmail.com>
2019-06-25 20:13:34 +0800

    两两交换链表中的节点

---
+++ 24.两两交换链表中的节点.java
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=java
+ *
+ * [24] 两两交换链表中的节点
+ *
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (60.52%)
+ * Likes:    225
+ * Dislikes: 0
+ * Total Accepted:    25.7K
+ * Total Submissions: 42.4K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * 
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode result = head.next;
+
+        ListNode pre = null;
+
+        while (head != null && head.next != null) {
+            ListNode second = head.next;
+            head.next = second.next;
+            second.next = head;
+
+            if (pre != null) {
+                pre.next = second;
+            }
+
+            pre = head;
+
+            head = head.next;
+        }
+
+        return result;
+    }
+}
+
