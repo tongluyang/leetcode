24f06460a251a52e778f9c890c0e83ad1e70dbdc
tongluyang <tongluyang@gmail.com>
2019-07-04 13:00:34 +0800

    搜索旋转排序数组

---
+++ 33.搜索旋转排序数组.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=java
+ *
+ * [33] 搜索旋转排序数组
+ *
+ * https://leetcode-cn.com/problems/search-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (36.59%)
+ * Likes:    280
+ * Dislikes: 0
+ * Total Accepted:    29.1K
+ * Total Submissions: 79.7K
+ * Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+ *
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ * 
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ * 
+ * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+ * 
+ * 你可以假设数组中不存在重复的元素。
+ * 
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums = [4,5,6,7,0,1,2], target = 0
+ * 输出: 4
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums = [4,5,6,7,0,1,2], target = 3
+ * 输出: -1
+ * 
+ */
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+            if (i + 1 < nums.length && nums[i] > nums[i + 1]) {
+                if (nums[i] < target || nums[i + 1] > target) {
+                    return -1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
