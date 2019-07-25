69c29d2b90b52fd0a186b25e4eeef63da1ec986d
tongluyang <tongluyang@gmail.com>
2019-07-25 19:17:01 +0800

    字母异位词分组

---
+++ 49.字母异位词分组.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=49 lang=java
+ *
+ * [49] 字母异位词分组
+ *
+ * https://leetcode-cn.com/problems/group-anagrams/description/
+ *
+ * algorithms
+ * Medium (56.45%)
+ * Likes:    166
+ * Dislikes: 0
+ * Total Accepted:    21.5K
+ * Total Submissions: 37.5K
+ * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+ *
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ * 
+ * 示例:
+ * 
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * 输出:
+ * [
+ * ⁠ ["ate","eat","tea"],
+ * ⁠ ["nat","tan"],
+ * ⁠ ["bat"]
+ * ]
+ * 
+ * 说明：
+ * 
+ * 
+ * 所有输入均为小写字母。
+ * 不考虑答案输出的顺序。
+ * 
+ * 
+ */
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            List<String> strings = map.computeIfAbsent(String.valueOf(chars), v -> new ArrayList<>());
+            strings.add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
+
