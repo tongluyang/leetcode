6e7517b6522f01b7f1c84a5e413aca31257e3cc0
tongluyang <tongluyang@gmail.com>
2019-07-25 18:53:48 +0800

    旋转图像 转置翻转

--- 48.旋转图像.java
+++ 48.旋转图像.java
@@ -62,15 +62,18 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
-        int[][] tmp = new int[n][n];
         for (int i = 0; i < n; i++) {
-            System.arraycopy(matrix[i], 0, tmp[i], 0, n);
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
         }
-
         for (int i = 0; i < n; i++) {
-            int[] row = matrix[i];
-            for (int j = 0; j < n; j++) {
-                row[j] = tmp[n - j - 1][i];
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
             }
         }
     }
