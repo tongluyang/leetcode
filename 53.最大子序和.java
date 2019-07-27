b8c213c17377c7efbcd8b14f0da266982a7c757a
tongluyang <tongluyang@gmail.com>
2019-07-27 09:51:04 +0800

    最大子序和 O(n)

---
+++ 53.最大子序和.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=java
+ *
+ * [53] 最大子序和
+ *
+ * https://leetcode-cn.com/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Easy (45.66%)
+ * Likes:    1064
+ * Dislikes: 0
+ * Total Accepted:    73.3K
+ * Total Submissions: 158.1K
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 
+ * 示例:
+ * 
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ * 
+ * 
+ * 进阶:
+ * 
+ * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+ * 
+ */
+class Solution {
+    public int maxSubArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i], nums[i] + (i - 1 >= 0 ? nums[i - 1] : 0));
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(nums[i], max);
+        }
+        return max;
+    }
+}
+
