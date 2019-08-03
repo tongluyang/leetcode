f2b276f2a7e8308eb93ce5b6c92611c4573f1c9d
tongluyang <tongluyang@gmail.com>
2019-08-03 13:52:08 +0800

    不同路径

---
+++ 62.不同路径.java
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=java
+ *
+ * [62] 不同路径
+ *
+ * https://leetcode-cn.com/problems/unique-paths/description/
+ *
+ * algorithms
+ * Medium (55.09%)
+ * Likes:    285
+ * Dislikes: 0
+ * Total Accepted:    29.5K
+ * Total Submissions: 53.5K
+ * Testcase Example:  '3\n2'
+ *
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * 
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * 
+ * 问总共有多少条不同的路径？
+ * 
+ * 
+ * 
+ * 例如，上图是一个7 x 3 的网格。有多少可能的路径？
+ * 
+ * 说明：m 和 n 的值均不超过 100。
+ * 
+ * 示例 1:
+ * 
+ * 输入: m = 3, n = 2
+ * 输出: 3
+ * 解释:
+ * 从左上角开始，总共有 3 条路径可以到达右下角。
+ * 1. 向右 -> 向右 -> 向下
+ * 2. 向右 -> 向下 -> 向右
+ * 3. 向下 -> 向右 -> 向右
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: m = 7, n = 3
+ * 输出: 28
+ * 
+ */
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        int[][] dp = new int[n][m];
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                if (row == 0) {
+                    dp[row][col] = 1;
+                } else {
+                    dp[row][col] = dp[row - 1][col] + (col == 0 ? 0 : dp[row][col - 1]);
+                }
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+}
+
