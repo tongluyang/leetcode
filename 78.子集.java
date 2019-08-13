cf23b6a3c61d9bb238b2b93ae06bf872b91ef4f9
tongluyang <tongluyang@gmail.com>
2019-08-13 19:44:22 +0800

    子集

---
+++ 78.子集.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=java
+ *
+ * [78] 子集
+ *
+ * https://leetcode-cn.com/problems/subsets/description/
+ *
+ * algorithms
+ * Medium (74.27%)
+ * Likes:    302
+ * Dislikes: 0
+ * Total Accepted:    30K
+ * Total Submissions: 40.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * 
+ * 说明：解集不能包含重复的子集。
+ * 
+ * 示例:
+ * 
+ * 输入: nums = [1,2,3]
+ * 输出:
+ * [
+ * ⁠ [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
+ * 
+ */
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> single = new ArrayList<>();
+        result.add(single);
+
+        for (int i = 0; i < nums.length; i++) {
+            int size = result.size();
+            for (int j = 0; j < size; j++) {
+                List<Integer> integers = result.get(j);
+                ArrayList<Integer> n = new ArrayList<>(integers);
+                n.add(nums[i]);
+                result.add(n);
+            }
+        }
+        return result;
+    }
+}
+
