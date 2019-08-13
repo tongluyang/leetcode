b471b2c4578d0bc7c4da22284b8a9afa3c091830
tongluyang <tongluyang@gmail.com>
2019-08-13 18:45:35 +0800

    组合

---
+++ 77.组合.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=77 lang=java
+ *
+ * [77] 组合
+ *
+ * https://leetcode-cn.com/problems/combinations/description/
+ *
+ * algorithms
+ * Medium (69.75%)
+ * Likes:    143
+ * Dislikes: 0
+ * Total Accepted:    16.3K
+ * Total Submissions: 23.4K
+ * Testcase Example:  '4\n2'
+ *
+ * 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+ * 
+ * 示例:
+ * 
+ * 输入: n = 4, k = 2
+ * 输出:
+ * [
+ * ⁠ [2,4],
+ * ⁠ [3,4],
+ * ⁠ [2,3],
+ * ⁠ [1,2],
+ * ⁠ [1,3],
+ * ⁠ [1,4],
+ * ]
+ * 
+ */
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        combine(result, new ArrayList<>(), n, k);
+
+        return result;
+    }
+
+    public void combine(List<List<Integer>> result, List<Integer> single, int n, int k) {
+        if (single.size() == k) {
+            result.add(single);
+            return;
+        }
+
+        for (int i = single.isEmpty() ? 1 : single.get(single.size() - 1) + 1; i <= n; i++) {
+            ArrayList<Integer> newSingle = new ArrayList<>(single);
+            newSingle.add(i);
+            combine(result, newSingle, n, k);
+        }
+    }
+}
+
