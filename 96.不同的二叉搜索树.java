6ecfe9714fdb3d8df692cdd2c1d8dd3c94107cca
tongluyang <tongluyang@gmail.com>
2019-08-31 15:05:44 +0800

    不同的二叉搜索树

---
+++ 96.不同的二叉搜索树.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=96 lang=java
+ *
+ * [96] 不同的二叉搜索树
+ *
+ * https://leetcode-cn.com/problems/unique-binary-search-trees/description/
+ *
+ * algorithms
+ * Medium (61.47%)
+ * Likes:    239
+ * Dislikes: 0
+ * Total Accepted:    14.6K
+ * Total Submissions: 23.5K
+ * Testcase Example:  '3'
+ *
+ * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+ * 
+ * 示例:
+ * 
+ * 输入: 3
+ * 输出: 5
+ * 解释:
+ * 给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+ * 
+ * ⁠  1         3     3      2      1
+ * ⁠   \       /     /      / \      \
+ * ⁠    3     2     1      1   3      2
+ * ⁠   /     /       \                 \
+ * ⁠  2     1         2                 3
+ * 
+ */
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                dp[i + 1] += dp[i - j] * dp[j];
+            }
+        }
+        return dp[n];
+    }
+}
+
