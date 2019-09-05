14406c22f6719580ecd6abe3dfc465f092435c33
tongluyang <tongluyang@gmail.com>
2019-09-05 13:28:43 +0800

    二叉树的锯齿形层次遍历

---
+++ 103.二叉树的锯齿形层次遍历.java
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=103 lang=java
+ *
+ * [103] 二叉树的锯齿形层次遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (50.70%)
+ * Likes:    90
+ * Dislikes: 0
+ * Total Accepted:    17.1K
+ * Total Submissions: 33.2K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ * 
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 返回锯齿形层次遍历如下：
+ * 
+ * [
+ * ⁠ [3],
+ * ⁠ [20,9],
+ * ⁠ [15,7]
+ * ]
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        zigzagLevelOrder(result, root);
+        return result;
+    }
+
+    private void zigzagLevelOrder(List<List<Integer>> result, TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int level = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> row = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                assert node != null;
+                row.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            if (level % 2 == 0) {
+                Collections.reverse(row);
+            }
+            result.add(row);
+            level++;
+        }
+    }
+}
+
