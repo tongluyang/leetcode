b318b70efe8b9da7e2a2d302dec3029219d9dd2e
tongluyang <tongluyang@gmail.com>
2019-09-30 13:14:22 +0800

    被围绕的区域

---
+++ 130.被围绕的区域.java
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=130 lang=java
+ *
+ * [130] 被围绕的区域
+ *
+ * https://leetcode-cn.com/problems/surrounded-regions/description/
+ *
+ * algorithms
+ * Medium (37.22%)
+ * Likes:    104
+ * Dislikes: 0
+ * Total Accepted:    10.8K
+ * Total Submissions: 28.4K
+ * Testcase Example:  '[["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]'
+ *
+ * 给定一个二维的矩阵，包含 'X' 和 'O'（字母 O）。
+ * 
+ * 找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。
+ * 
+ * 示例:
+ * 
+ * X X X X
+ * X O O X
+ * X X O X
+ * X O X X
+ * 
+ * 
+ * 运行你的函数后，矩阵变为：
+ * 
+ * X X X X
+ * X X X X
+ * X X X X
+ * X O X X
+ * 
+ * 
+ * 解释:
+ * 
+ * 被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，或不与边界上的 'O' 相连的 'O'
+ * 最终都会被填充为 'X'。如果两个元素在水平或垂直方向相邻，则称它们是“相连”的。
+ * 
+ */
+class Solution {
+    public void solve(char[][] board) {
+        int row = board.length;
+        if (row <= 1 || board[0].length <= 1) {
+            return;
+        }
+        int col = board[0].length;
+
+        for (int j = 0; j < col; j++) {
+            solve(board, 0, j);
+            solve(board, row - 1, j);
+        }
+
+        for (int i = 0; i < row; i++) {
+            solve(board, i, 0);
+            solve(board, i, col - 1);
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    private void solve(char[][] board, int i, int j) {
+        if (i == -1 || j == -1 || board.length == i || board[0].length == j) {
+            return;
+        }
+        if (board[i][j] == 'O') {
+            board[i][j] = '#';
+            solve(board, i + 1, j);
+            solve(board, i - 1, j);
+            solve(board, i, j + 1);
+            solve(board, i, j - 1);
+        }
+    }
+}
+
