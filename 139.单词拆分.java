a01b661882ce25caaa0378a623cdd2bd586620b1
tongluyang <tongluyang@gmail.com>
2019-10-15 19:50:25 +0800

    单词拆分

---
+++ 139.单词拆分.java
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=139 lang=java
+ *
+ * [139] 单词拆分
+ *
+ * https://leetcode-cn.com/problems/word-break/description/
+ *
+ * algorithms
+ * Medium (42.22%)
+ * Likes:    214
+ * Dislikes: 0
+ * Total Accepted:    21.4K
+ * Total Submissions: 50.1K
+ * Testcase Example:  '"leetcode"\n["leet","code"]'
+ *
+ * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ * 
+ * 说明：
+ * 
+ * 
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+ * 注意你可以重复使用字典中的单词。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return wordBreak(s, 0, new HashSet<>(wordDict), new Boolean[s.length()]);
+    }
+
+    private boolean wordBreak(String s, int start, Set<String> wordDict, Boolean[] mem) {
+        if (start == s.length()) {
+            return true;
+        }
+        if (mem[start] != null) {
+            return mem[start];
+        }
+        int length = 1;
+        while (start + length <= s.length()) {
+            String str = s.substring(start, start + length);
+            if (wordDict.contains(str)) {
+                if (wordBreak(s, start + str.length(), wordDict, mem)) {
+                    return mem[start] = true;
+                }
+            }
+            length++;
+        }
+        return mem[start] = false;
+    }
+}
+// @lc code=end
+
