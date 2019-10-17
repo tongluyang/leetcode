d4d3e38cc379abe60bb90dad6335cd474ad2a345
tongluyang <tongluyang@gmail.com>
2019-10-17 13:20:45 +0800

    环形链表

---
+++ 141.环形链表.java
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ *
+ * [141] 环形链表
+ *
+ * https://leetcode-cn.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (41.86%)
+ * Likes:    395
+ * Dislikes: 0
+ * Total Accepted:    73.1K
+ * Total Submissions: 167.5K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * 给定一个链表，判断链表中是否有环。
+ * 
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * 
+ * 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * 
+ * 
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 你能用 O(1)（即，常量）内存解决此问题吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        if (head.next == null) {
+            return false;
+        }
+        ListNode next = head.next;
+        ListNode nextNext = head.next.next;
+        while (next != null && nextNext != null) {
+            if (next == nextNext) {
+                return true;
+            }
+            next = next.next;
+            if (nextNext.next == null) {
+                return false;
+            }
+            nextNext = nextNext.next.next;
+        }
+        return false;
+    }
+}
+// @lc code=end
+
