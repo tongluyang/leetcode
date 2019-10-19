95fa07f1d018384da929fdd2d2d4c55781074453
tongluyang <tongluyang@gmail.com>
2019-10-19 13:35:30 +0800

    二叉树的后序遍历

---
+++ 145.二叉树的后序遍历.java
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=java
+ *
+ * [145] 二叉树的后序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Hard (66.91%)
+ * Likes:    175
+ * Dislikes: 0
+ * Total Accepted:    34K
+ * Total Submissions: 49.9K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树，返回它的 后序 遍历。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,null,2,3]  
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3 
+ * 
+ * 输出: [3,2,1]
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        postorderTraversal(result, root);
+        return result;
+    }
+
+    private void postorderTraversal(List<Integer> result, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postorderTraversal(result, root.left);
+        postorderTraversal(result, root.right);
+        result.add(root.val);
+    }
+}
+// @lc code=end
+
