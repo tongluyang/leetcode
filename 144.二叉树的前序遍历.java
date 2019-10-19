712b9e0bf21c7c01ff9a7745296d7a1f5100f12a
tongluyang <tongluyang@gmail.com>
2019-10-19 13:18:30 +0800

    二叉树的前序遍历

---
+++ 144.二叉树的前序遍历.java
@@ -0,0 +1,63 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=144 lang=java
+ *
+ * [144] 二叉树的前序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Medium (60.79%)
+ * Likes:    160
+ * Dislikes: 0
+ * Total Accepted:    44.7K
+ * Total Submissions: 71.9K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树，返回它的 前序 遍历。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,null,2,3]  
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3 
+ * 
+ * 输出: [1,2,3]
+ * 
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorderTraversal(result, root);
+        return result;
+    }
+
+    private void preorderTraversal(List<Integer> result, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        result.add(root.val);
+        preorderTraversal(result, root.left);
+        preorderTraversal(result, root.right);
+    }
+}
+// @lc code=end
+
