dc1dd9ecbb4cd4c4cc0c1d5d58597dcd94f374fa
tongluyang <tongluyang@gmail.com>
2019-10-29 13:42:57 +0800

    最小栈

---
+++ 155.最小栈.java
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=155 lang=java
+ *
+ * [155] 最小栈
+ *
+ * https://leetcode-cn.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (49.33%)
+ * Likes:    306
+ * Dislikes: 0
+ * Total Accepted:    51.3K
+ * Total Submissions: 101.8K
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+ * 
+ * 
+ * push(x) -- 将元素 x 推入栈中。
+ * pop() -- 删除栈顶的元素。
+ * top() -- 获取栈顶元素。
+ * getMin() -- 检索栈中的最小元素。
+ * 
+ * 
+ * 示例:
+ * 
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ * 
+ * 
+ */
+
+// @lc code=start
+class MinStack {
+
+    Stack<Integer> data = new Stack<>();
+    Stack<Integer> min = new Stack<>();
+    /** initialize your data structure here. */
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+        data.push(x);
+        if (min.isEmpty() || min.peek() > x) {
+            min.push(x);
+        } else {
+            min.push(min.peek());
+        }
+    }
+
+    public void pop() {
+        data.pop();
+        min.pop();
+    }
+
+    public int top() {
+        return data.peek();
+    }
+
+    public int getMin() {
+        return min.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+// @lc code=end
+
