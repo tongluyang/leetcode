84b2265ae005349c61a9737b537f14d8d233cdfc
tongluyang <tongluyang@gmail.com>
2019-11-04 13:15:41 +0800

    Excel表列序号

---
+++ 171.excel表列序号.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=171 lang=java
+ *
+ * [171] Excel表列序号
+ *
+ * https://leetcode-cn.com/problems/excel-sheet-column-number/description/
+ *
+ * algorithms
+ * Easy (64.90%)
+ * Likes:    87
+ * Dislikes: 0
+ * Total Accepted:    23.7K
+ * Total Submissions: 36.3K
+ * Testcase Example:  '"A"'
+ *
+ * 给定一个Excel表格中的列名称，返回其相应的列序号。
+ * 
+ * 例如，
+ * 
+ * ⁠   A -> 1
+ * ⁠   B -> 2
+ * ⁠   C -> 3
+ * ⁠   ...
+ * ⁠   Z -> 26
+ * ⁠   AA -> 27
+ * ⁠   AB -> 28 
+ * ⁠   ...
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: "A"
+ * 输出: 1
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: "AB"
+ * 输出: 28
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: "ZY"
+ * 输出: 701
+ * 
+ * 致谢：
+ * 特别感谢 @ts 添加此问题并创建所有测试用例。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int titleToNumber(String s) {
+        char[] chars = s.toCharArray();
+        int result = 0;
+        int base = 1;
+        for (int i = chars.length - 1; i >= 0; i--) {
+            int value = (chars[i] - 'A' + 1) * base;
+            result += value;
+            base *= 26;
+        }
+        return result;
+    }
+}
+// @lc code=end
+
