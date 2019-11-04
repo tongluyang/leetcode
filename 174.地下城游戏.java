c1244fa5de25e4d292390d9df032627bb3f9b98f
tongluyang <tongluyang@gmail.com>
2019-11-04 19:17:33 +0800

    地下城游戏

---
+++ 174.地下城游戏.java
@@ -0,0 +1,104 @@
+/*
+ * @lc app=leetcode.cn id=174 lang=java
+ *
+ * [174] 地下城游戏
+ *
+ * https://leetcode-cn.com/problems/dungeon-game/description/
+ *
+ * algorithms
+ * Hard (37.32%)
+ * Likes:    127
+ * Dislikes: 0
+ * Total Accepted:    4.9K
+ * Total Submissions: 12.8K
+ * Testcase Example:  '[[-2,-3,3],[-5,-10,1],[10,30,-5]]'
+ *
+ * 
+ * table.dungeon, .dungeon th, .dungeon td {
+ * ⁠ border:3px solid black;
+ * }
+ * 
+ * ⁠.dungeon th, .dungeon td {
+ * ⁠   text-align: center;
+ * ⁠   height: 70px;
+ * ⁠   width: 70px;
+ * }
+ * 
+ * 
+ * 一些恶魔抓住了公主（P）并将她关在了地下城的右下角。地下城是由 M x N
+ * 个房间组成的二维网格。我们英勇的骑士（K）最初被安置在左上角的房间里，他必须穿过地下城并通过对抗恶魔来拯救公主。
+ * 
+ * 骑士的初始健康点数为一个正整数。如果他的健康点数在某一时刻降至 0 或以下，他会立即死亡。
+ * 
+ * 有些房间由恶魔守卫，因此骑士在进入这些房间时会失去健康点数（若房间里的值为负整数，则表示骑士将损失健康点数）；其他房间要么是空的（房间里的值为
+ * 0），要么包含增加骑士健康点数的魔法球（若房间里的值为正整数，则表示骑士将增加健康点数）。
+ * 
+ * 为了尽快到达公主，骑士决定每次只向右或向下移动一步。
+ * 
+ * 
+ * 
+ * 编写一个函数来计算确保骑士能够拯救到公主所需的最低初始健康点数。
+ * 
+ * 例如，考虑到如下布局的地下城，如果骑士遵循最佳路径 右 -> 右 -> 下 -> 下，则骑士的初始健康点数至少为 7。
+ * 
+ * 
+ * ⁠
+ * -2 (K) 
+ * -3 
+ * 3 
+ * ⁠
+ * ⁠
+ * -5 
+ * -10 
+ * 1 
+ * ⁠
+ * ⁠
+ * 10 
+ * 30 
+ * -5 (P) 
+ * ⁠
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 说明:
+ * 
+ * 
+ * 
+ * 骑士的健康点数没有上限。
+ * 
+ * 任何房间都可能对骑士的健康点数造成威胁，也可能增加骑士的健康点数，包括骑士进入的左上角房间以及公主被监禁的右下角房间。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int row = dungeon.length;
+        int col = dungeon[0].length;
+        int[][] dp = new int[row + 1][col + 1];
+        for (int i = 0; i < row; i++) {
+            dp[i][col] = Integer.MAX_VALUE;
+        }
+        for (int j = 0; j < col; j++) {
+            dp[row][j] = Integer.MAX_VALUE;
+        }
+        dp[row - 1][col] = 1;
+        dp[row][col - 1] = 1;
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 0; j--) {
+                int val = dungeon[i][j];
+                int nextMin = Math.min(dp[i + 1][j], dp[i][j + 1]);
+                if (val >= nextMin) {//大血瓶，有命来就行
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = nextMin - val;
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
+// @lc code=end
+
