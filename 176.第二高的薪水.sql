8e65f0636ca2437e0df1b0d67c6bd8f0643440c4
tongluyang <tongluyang@gmail.com>
2019-11-05 12:49:35 +0800

    第二高的薪水

--- 176.第二高的薪水.sql
+++ 176.第二高的薪水.sql
@@ -1,3 +1,11 @@
 /* Write your PL/SQL query statement below */
 SELECT MAX(salary) "SecondHighestSalary" FROM employee 
 WHERE salary < (SELECT MAX(salary) FROM employee);
+
+/* Write your PL/SQL query statement below */
+/* SELECT sum(salary) "SecondHighestSalary" FROM (
+    SELECT salary, rownum rn FROM (
+        SELECT DISTINCT salary FROM employee ORDER BY salary desc
+    )
+)
+WHERE rn = 2; */
