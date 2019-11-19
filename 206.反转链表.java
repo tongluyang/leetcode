a99801bca5b2279b0a48613e9b134a1f49bfb9a5
tongluyang <tongluyang@gmail.com>
2019-11-19 12:46:09 +0800

    反转链表

---
+++ 206.反转链表.java
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=java
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (63.35%)
+ * Likes:    639
+ * Dislikes: 0
+ * Total Accepted:    118.4K
+ * Total Submissions: 180.8K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 反转一个单链表。
+ * 
+ * 示例:
+ * 
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode newNext = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newNext;
+            newNext = head;
+            head = next;
+        }
+        return newNext;
+    }
+}
+// @lc code=end
+
