5b29d642fdbbf0792a991a0a09cc2dc6ed704711
tongluyang <tongluyang@gmail.com>
2019-12-02 12:59:02 +0800

    存在重复元素

---
+++ 217.存在重复元素.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=217 lang=java
+ *
+ * [217] 存在重复元素
+ *
+ * https://leetcode-cn.com/problems/contains-duplicate/description/
+ *
+ * algorithms
+ * Easy (49.36%)
+ * Likes:    193
+ * Dislikes: 0
+ * Total Accepted:    92.7K
+ * Total Submissions: 182.6K
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * 给定一个整数数组，判断是否存在重复元素。
+ * 
+ * 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,2,3,1]
+ * 输出: true
+ * 
+ * 示例 2:
+ * 
+ * 输入: [1,2,3,4]
+ * 输出: false
+ * 
+ * 示例 3:
+ * 
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+            if (set.size() != i + 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
