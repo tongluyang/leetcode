610de864b6bae8843c935c6545bb744b7b10131c
tongluyang <tongluyang@gmail.com>
2019-12-18 13:54:51 +0800

    滑动窗口最大值

---
+++ 239.滑动窗口最大值.java
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=239 lang=java
+ *
+ * [239] 滑动窗口最大值
+ *
+ * https://leetcode-cn.com/problems/sliding-window-maximum/description/
+ *
+ * algorithms
+ * Hard (42.09%)
+ * Likes:    185
+ * Dislikes: 0
+ * Total Accepted:    22.3K
+ * Total Submissions: 51.7K
+ * Testcase Example:  '[1,3,-1,-3,5,3,6,7]\n3'
+ *
+ * 给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k
+ * 个数字。滑动窗口每次只向右移动一位。
+ * 
+ * 返回滑动窗口中的最大值。
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+ * 输出: [3,3,5,5,6,7] 
+ * 解释: 
+ * 
+ * ⁠ 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * ⁠1 [3  -1  -3] 5  3  6  7       3
+ * ⁠1  3 [-1  -3  5] 3  6  7       5
+ * ⁠1  3  -1 [-3  5  3] 6  7       5
+ * ⁠1  3  -1  -3 [5  3  6] 7       6
+ * ⁠1  3  -1  -3  5 [3  6  7]      7
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 你能在线性时间复杂度内解决此题吗？
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0) {
+            return new int[0];
+        }
+        int[] result = new int[nums.length - k + 1];
+        int start = 0;
+        int end = start + k - 1;
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
+        for (int i = 0; i < k - 1; i++) {
+            queue.add(nums[i]);
+        }
+        for (int i = 0; i < result.length; i++) {
+            if (start > 0) {
+                queue.remove(nums[start - 1]);
+            }
+            queue.add(nums[end]);
+            result[i] = queue.peek();
+            start++;
+            end++;
+        }
+        return result;
+    }
+}
+// @lc code=end
+
