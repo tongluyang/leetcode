a660b4f6f75345797e8a152f86f4d52e50273b60
tongluyang <tongluyang@gmail.com>
2019-12-21 09:36:00 +0800

    有效的字母异位词 hashmap

---
+++ 242.有效的字母异位词.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=java
+ *
+ * [242] 有效的字母异位词
+ *
+ * https://leetcode-cn.com/problems/valid-anagram/description/
+ *
+ * algorithms
+ * Easy (54.63%)
+ * Likes:    137
+ * Dislikes: 0
+ * Total Accepted:    62.9K
+ * Total Submissions: 109.6K
+ * Testcase Example:  '"anagram"\n"nagaram"'
+ *
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ * 
+ * 示例 1:
+ * 
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ * 
+ * 说明:
+ * 你可以假设字符串只包含小写字母。
+ * 
+ * 进阶:
+ * 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+        }
+        for (Integer value : map.values()) {
+            if (value != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
+
