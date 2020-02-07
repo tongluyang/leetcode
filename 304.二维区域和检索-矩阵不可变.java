1e54a30b06aa311a8e5032e7bf0c690e91bb7a6a
tongluyang <tongluyang@gmail.com>
2020-02-07 17:50:15 +0800

    二维区域和检索 - 矩阵不可变

---
+++ 304.二维区域和检索-矩阵不可变.java
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=304 lang=java
+ *
+ * [304] 二维区域和检索 - 矩阵不可变
+ *
+ * https://leetcode-cn.com/problems/range-sum-query-2d-immutable/description/
+ *
+ * algorithms
+ * Medium (42.13%)
+ * Likes:    56
+ * Dislikes: 0
+ * Total Accepted:    5.4K
+ * Total Submissions: 12.8K
+ * Testcase Example:  '["NumMatrix","sumRegion","sumRegion","sumRegion"]\n' +
+  '[[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]'
+ *
+ * 给定一个二维矩阵，计算其子矩形范围内元素的总和，该子矩阵的左上角为 (row1, col1) ，右下角为 (row2, col2)。
+ * 
+ * 
+ * 上图子矩阵左上角 (row1, col1) = (2, 1) ，右下角(row2, col2) = (4, 3)，该子矩形内元素的总和为 8。
+ * 
+ * 示例:
+ * 
+ * 给定 matrix = [
+ * ⁠ [3, 0, 1, 4, 2],
+ * ⁠ [5, 6, 3, 2, 1],
+ * ⁠ [1, 2, 0, 1, 5],
+ * ⁠ [4, 1, 0, 1, 7],
+ * ⁠ [1, 0, 3, 0, 5]
+ * ]
+ * 
+ * sumRegion(2, 1, 4, 3) -> 8
+ * sumRegion(1, 1, 2, 2) -> 11
+ * sumRegion(1, 2, 2, 4) -> 12
+ * 
+ * 
+ * 说明:
+ * 
+ * 
+ * 你可以假设矩阵不可变。
+ * 会多次调用 sumRegion 方法。
+ * 你可以假设 row1 ≤ row2 且 col1 ≤ col2。
+ * 
+ * 
+ */
+
+// @lc code=start
+class NumMatrix {
+
+    int[][] sums;
+    public NumMatrix(int[][] matrix) {
+        int row = matrix.length;
+        if (row == 0) {
+            return;
+        }
+        int col = matrix[0].length;
+        if (col == 0) {
+            return;
+        }
+        sums = new int[row + 1][col + 1];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                sums[i + 1][j + 1] = sums[i][j + 1] + sums[i + 1][j] - sums[i][j] + matrix[i][j];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2 + 1][col2 + 1] - sums[row1][col2 + 1] - sums[row2 + 1][col1] + sums[row1][col1];
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
+// @lc code=end
+
