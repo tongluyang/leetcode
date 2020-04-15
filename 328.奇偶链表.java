39741cd6da718a5a5945d31a0bbd5ffda37ee348
tongluyang <tongluyang@gmail.com>
2020-04-15 13:46:59 +0800

    奇偶链表 stack

---
+++ 328.奇偶链表.java
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=328 lang=java
+ *
+ * [328] 奇偶链表
+ *
+ * https://leetcode-cn.com/problems/odd-even-linked-list/description/
+ *
+ * algorithms
+ * Medium (61.83%)
+ * Likes:    159
+ * Dislikes: 0
+ * Total Accepted:    33.2K
+ * Total Submissions: 53.4K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+ * 
+ * 请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 1->3->5->2->4->NULL
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: 2->1->3->5->6->4->7->NULL 
+ * 输出: 2->3->6->7->1->5->4->NULL
+ * 
+ * 说明:
+ * 
+ * 
+ * 应当保持奇数节点和偶数节点的相对顺序。
+ * 链表的第一个节点视为奇数节点，第二个节点视为偶数节点，以此类推。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        if (even == null) {
+            return head;
+        }
+        ListNode evenHead = head.next;
+
+        while (even != null && even.next != null) {
+            ListNode actionNode = even.next;
+            //原位置删除
+            even.next = actionNode.next;
+            even = even.next;
+
+            //新位置插入
+            odd.next = actionNode;
+            actionNode.next = evenHead;
+            odd = odd.next;
+        }
+        return head;
+    }
+}
+// @lc code=end
+
