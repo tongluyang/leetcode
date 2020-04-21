1a3f5175a5ec0dbc07cdfdaf96058b7c29ca2191
tongluyang <tongluyang@gmail.com>
2020-04-21 13:09:16 +0800

    统计「优美子数组」

---
+++ 1248.统计「优美子数组」.java
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=1248 lang=java
+ *
+ * [1248] 统计「优美子数组」
+ *
+ * https://leetcode-cn.com/problems/count-number-of-nice-subarrays/description/
+ *
+ * algorithms
+ * Medium (47.43%)
+ * Likes:    55
+ * Dislikes: 0
+ * Total Accepted:    8.5K
+ * Total Submissions: 16.2K
+ * Testcase Example:  '[1,1,2,1,1]\n3'
+ *
+ * 给你一个整数数组 nums 和一个整数 k。
+ * 
+ * 如果某个 连续 子数组中恰好有 k 个奇数数字，我们就认为这个子数组是「优美子数组」。
+ * 
+ * 请返回这个数组中「优美子数组」的数目。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums = [1,1,2,1,1], k = 3
+ * 输出：2
+ * 解释：包含 3 个奇数的子数组是 [1,1,2,1] 和 [1,2,1,1] 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：nums = [2,4,6], k = 1
+ * 输出：0
+ * 解释：数列中不包含任何奇数，所以不存在优美子数组。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：nums = [2,2,2,1,2,2,1,2,2,2], k = 2
+ * 输出：16
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 50000
+ * 1 <= nums[i] <= 10^5
+ * 1 <= k <= nums.length
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int res = 0;
+        int p0 = 0;
+        int p1 = 0;
+        int count = 0;
+        int left = 1;
+        int right = 1;
+        while (p1 < nums.length || count == k) {
+            if (count == k) {
+                while (nums[p0] % 2 == 0) {
+                    left++;
+                    p0++;
+                }
+                while (p1 < nums.length && nums[p1] % 2 == 0) {
+                    right++;
+                    p1++;
+                }
+                res += left * right;
+                p0++;
+                count--;
+                left = 1;
+                right = 1;
+            } else if (count < k) {
+                if (nums[p1] % 2 == 1) {
+                    count++;
+                }
+                p1++;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
