2009f5d0e24155f1fc9cdfc5dc1306c7646e9f8f
tongluyang <tongluyang@gmail.com>
2020-05-01 09:47:03 +0800

    合并两个有序链表

--- 21.合并两个有序链表.java
+++ 21.合并两个有序链表.java
@@ -32,46 +32,18 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-
         if (l1 == null) {
             return l2;
         }
-
         if (l2 == null) {
             return l1;
         }
-
-        ListNode l = null;
-        ListNode end = null;
-        do {
-            int val;
-
-            if (l1 == null) {
-                end.next = l2;
-                break;
-            } else if (l2 == null) {
-                end.next = l1;
-                break;
-            }
-
-            if (l1.val < l2.val) {
-                val = l1.val;
-                l1 = l1.next;
-            } else {
-                val = l2.val;
-                l2 = l2.next;
-            }
-
-            if (l == null) {
-                l = new ListNode(val);
-                end = l;
-            } else {
-                end.next = new ListNode(val);
-                end = end.next;
-            }
-
-        } while (true);
-        return l;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
     }
 }
 
