a70a5b559b053da42bb5ad1947c18ea587be295b
tongluyang <tongluyang@gmail.com>
2020-05-07 13:18:33 +0800

    另一个树的子树

---
+++ 572.另一个树的子树.java
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode.cn id=572 lang=java
+ *
+ * [572] 另一个树的子树
+ *
+ * https://leetcode-cn.com/problems/subtree-of-another-tree/description/
+ *
+ * algorithms
+ * Easy (44.59%)
+ * Likes:    216
+ * Dislikes: 0
+ * Total Accepted:    25.2K
+ * Total Submissions: 55K
+ * Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
+ *
+ * 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s
+ * 也可以看做它自身的一棵子树。
+ * 
+ * 示例 1:
+ * 给定的树 s:
+ * 
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * 
+ * 给定的树 t：
+ * 
+ * 
+ * ⁠  4 
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * 
+ * 返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+ * 
+ * 示例 2:
+ * 给定的树 s：
+ * 
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \
+ * ⁠1   2
+ * ⁠   /
+ * ⁠  0
+ * 
+ * 
+ * 给定的树 t：
+ * 
+ * 
+ * ⁠  4
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * 
+ * 返回 false。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return isSameTree(s, t) || (s != null && (isSubtree(s.left, t) || isSubtree(s.right, t)));
+    }
+    
+    private boolean isSameTree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null) {
+            return false;
+        }
+        if (t == null) {
+            return false;
+        }
+        if (s.val != t.val) {
+            return false;
+        }
+        return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+    }
+}
+// @lc code=end
+
