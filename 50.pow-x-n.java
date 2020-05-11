3f980ede51f86c1ab0f9da78bc4930f171e7d5a4
tongluyang <tongluyang@gmail.com>
2020-05-11 12:53:48 +0800

    Pow(x, n)

--- 50.pow-x-n.java
+++ 50.pow-x-n.java
@@ -43,38 +43,16 @@
  */
 class Solution {
     public double myPow(double x, int n) {
-        if (x == 0) {
-            return 0;
-        }
         if (n == 0) {
-            return 1;
-        }
-        if (x == 1) {
             return 1;
         }
         if (n == 1) {
             return x;
         }
-        if (x == -1) {
-            return n % 2 == 0 ? 1 : -1;
-        }
-        int abs = Math.abs(n);
-        if (abs < 0) {
-            return 0;
-        }
-
-        double result;
-
-        result = myPow(x, abs / 2);
-        result = result * result;
-
-        result = result * myPow(x, abs % 2);
-
         if (n < 0) {
-            return 1 / result;
+            return 1 / (myPow(x, -(n + 1)) * x);
         }
-
-        return result;
+        double x1 = myPow(x, n / 2);
+        return x1 * x1 * myPow(x, n % 2);
     }
 }
-
