211599df3a85e0ca8fc2c5e801c1c3657335ae75
tongluyang <tongluyang@gmail.com>
2020-05-24 22:15:49 +0800

    寻找两个有序数组的中位数

--- 4.寻找两个有序数组的中位数.java
+++ 4.寻找两个有序数组的中位数.java
@@ -36,30 +36,31 @@
  * 
  * 
  */
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        final int[] all = new int[nums1.length + nums2.length];
-        System.arraycopy(nums1, 0, all, 0, nums1.length);
-        System.arraycopy(nums2, 0, all, nums1.length, nums2.length);
-
-        final List<Integer> ints = new ArrayList<>();
-        for (int i : all) {
-            ints.add(i);
+        int len = nums1.length + nums2.length;
+        if (len % 2 == 0) {
+            return (getKth(nums1, nums2, 0, 0, len / 2) + getKth(nums1, nums2, 0, 0, len / 2 + 1)) / 2.0;
         }
+        return getKth(nums1, nums2, 0, 0, len / 2 + 1);
+    }
 
-        //O(log(m + n))
-        //快排
-        Collections.sort(ints);
-
-        if (ints.size() % 2 == 1) {
-            return ints.get((ints.size() - 1) / 2);
-        } else {
-            return (ints.get((ints.size() / 2)) + ints.get((ints.size() / 2 - 1))) / 2.0;
+    private int getKth(int[] nums1, int[] nums2, int start1, int start2, int k) {
+        if (start1 >= nums1.length) {
+            return nums2[start2 + k - 1];
         }
+        if (start2 >= nums2.length) {
+            return nums1[start1 + k - 1];
+        }
+        if (k == 1) {
+            return Math.min(nums1[start1], nums2[start2]);
+        }
+        int mid = k / 2;
+        int end1 = Math.min(start1 + mid - 1, nums1.length - 1);
+        int end2 = Math.min(start2 + mid - 1, nums2.length - 1);
+        if (nums1[end1] < nums2[end2]) {
+            return getKth(nums1, nums2, end1 + 1, start2, k - (end1 + 1 - start1));
+        }
+        return getKth(nums1, nums2, start1, end2 + 1, k - (end2 + 1 - start2));
     }
 }
-
