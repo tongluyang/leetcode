17ccb8d6c3bdcacdb0545e49699e044c6bafacf5
tongluyang <tongluyang@gmail.com>
2020-05-28 13:38:33 +0800

    链表随机节点 蓄水池抽样

--- 382.链表随机节点.java
+++ 382.链表随机节点.java
@@ -44,28 +44,26 @@
  */
 class Solution {
 
-    int len = 0;
     ListNode head;
     Random random = new Random();
     /** @param head The linked list's head.
         Note that the head is guaranteed to be not null, so it contains at least one node. */
     public Solution(ListNode head) {
-        ListNode cur = head;
-        while (cur != null) {
-            len++;
-            cur = cur.next;
-        }
         this.head = head;
     }
     
     /** Returns a random node's value. */
     public int getRandom() {
-        int r = random.nextInt(len);
+        int i = 1;
         ListNode cur = head;
-        for (int i = 0; i < r; i++) {
+        int res = 0;
+        while (cur != null) {
+            if (random.nextInt(i++) == 0) {
+                res = cur.val;
+            }
             cur = cur.next;
         }
-        return cur.val;
+        return res;
     }
 }
 
