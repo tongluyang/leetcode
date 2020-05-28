8ceaf19a6778095f2fdeac60017e626b798d87aa
tongluyang <tongluyang@gmail.com>
2020-05-28 13:06:04 +0800

    链表随机节点

---
+++ 382.链表随机节点.java
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=382 lang=java
+ *
+ * [382] 链表随机节点
+ *
+ * https://leetcode-cn.com/problems/linked-list-random-node/description/
+ *
+ * algorithms
+ * Medium (56.84%)
+ * Likes:    57
+ * Dislikes: 0
+ * Total Accepted:    4.9K
+ * Total Submissions: 8.7K
+ * Testcase Example:  '["Solution","getRandom"]\n[[[1,2,3]],[]]'
+ *
+ * 给定一个单链表，随机选择链表的一个节点，并返回相应的节点值。保证每个节点被选的概率一样。
+ * 
+ * 进阶:
+ * 如果链表十分大且长度未知，如何解决这个问题？你能否使用常数级空间复杂度实现？
+ * 
+ * 示例:
+ * 
+ * 
+ * // 初始化一个单链表 [1,2,3].
+ * ListNode head = new ListNode(1);
+ * head.next = new ListNode(2);
+ * head.next.next = new ListNode(3);
+ * Solution solution = new Solution(head);
+ * 
+ * // getRandom()方法应随机返回1,2,3中的一个，保证每个元素被返回的概率相等。
+ * solution.getRandom();
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    int len = 0;
+    ListNode head;
+    Random random = new Random();
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    public Solution(ListNode head) {
+        ListNode cur = head;
+        while (cur != null) {
+            len++;
+            cur = cur.next;
+        }
+        this.head = head;
+    }
+    
+    /** Returns a random node's value. */
+    public int getRandom() {
+        int r = random.nextInt(len);
+        ListNode cur = head;
+        for (int i = 0; i < r; i++) {
+            cur = cur.next;
+        }
+        return cur.val;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
+// @lc code=end
+
