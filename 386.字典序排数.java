31efc2e584aa388fa95d0f6460ade43b571f3b12
tongluyang <tongluyang@gmail.com>
2020-05-29 13:52:15 +0800

    字典序排数 排序

---
+++ 386.字典序排数.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=386 lang=java
+ *
+ * [386] 字典序排数
+ *
+ * https://leetcode-cn.com/problems/lexicographical-numbers/description/
+ *
+ * algorithms
+ * Medium (69.84%)
+ * Likes:    86
+ * Dislikes: 0
+ * Total Accepted:    8.7K
+ * Total Submissions: 12.4K
+ * Testcase Example:  '13'
+ *
+ * 给定一个整数 n, 返回从 1 到 n 的字典顺序。
+ * 
+ * 例如，
+ * 
+ * 给定 n =1 3，返回 [1,10,11,12,13,2,3,4,5,6,7,8,9] 。
+ * 
+ * 请尽可能的优化算法的时间复杂度和空间复杂度。 输入的数据 n 小于等于 5,000,000。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            res.add(i);
+        }
+        res.sort((a, b) -> String.valueOf(a).compareTo(String.valueOf(b)));
+        return res;
+    }
+}
+// @lc code=end
+
