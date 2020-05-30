29a05c47a2658217022ee814a005cdf12d306c40
tongluyang <tongluyang@gmail.com>
2020-05-30 13:43:54 +0800

    找不同

---
+++ 389.找不同.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=389 lang=java
+ *
+ * [389] 找不同
+ *
+ * https://leetcode-cn.com/problems/find-the-difference/description/
+ *
+ * algorithms
+ * Easy (61.76%)
+ * Likes:    125
+ * Dislikes: 0
+ * Total Accepted:    27.7K
+ * Total Submissions: 44.8K
+ * Testcase Example:  '"abcd"\n"abcde"'
+ *
+ * 给定两个字符串 s 和 t，它们只包含小写字母。
+ * 
+ * 字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+ * 
+ * 请找出在 t 中被添加的字母。
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * 输入：
+ * s = "abcd"
+ * t = "abcde"
+ * 
+ * 输出：
+ * e
+ * 
+ * 解释：
+ * 'e' 是那个被添加的字母。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] map = new int[26];
+        for (char c : s.toCharArray()) {
+            map[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            map[c - 'a']--;
+            if (map[c - 'a'] < 0) {
+                return c;
+            }
+        }
+        return 0;
+    }
+}
+// @lc code=end
+
