bebc4d0a19e787065e47a886f695c25625f3c713
tongluyang <tongluyang@gmail.com>
2020-05-30 11:18:15 +0800

    字符串中的第一个唯一字符

---
+++ 387.字符串中的第一个唯一字符.java
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=387 lang=java
+ *
+ * [387] 字符串中的第一个唯一字符
+ *
+ * https://leetcode-cn.com/problems/first-unique-character-in-a-string/description/
+ *
+ * algorithms
+ * Easy (45.01%)
+ * Likes:    212
+ * Dislikes: 0
+ * Total Accepted:    78.3K
+ * Total Submissions: 173.4K
+ * Testcase Example:  '"leetcode"'
+ *
+ * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+ * 
+ * 案例:
+ * 
+ * 
+ * s = "leetcode"
+ * 返回 0.
+ * 
+ * s = "loveleetcode",
+ * 返回 2.
+ * 
+ * 
+ * 
+ * 
+ * 注意事项：您可以假定该字符串只包含小写字母。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] map = new int[26];
+        for (char c : s.toCharArray()) {
+            map[c - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
+
