c3c6252fe8d4594fdc828dda25df0e46b25c8688
tongluyang <tongluyang@gmail.com>
2020-06-01 13:18:22 +0800

    消除游戏

---
+++ 390.消除游戏.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=390 lang=java
+ *
+ * [390] 消除游戏
+ *
+ * https://leetcode-cn.com/problems/elimination-game/description/
+ *
+ * algorithms
+ * Medium (43.47%)
+ * Likes:    64
+ * Dislikes: 0
+ * Total Accepted:    3.1K
+ * Total Submissions: 7.1K
+ * Testcase Example:  '9'
+ *
+ * 给定一个从1 到 n 排序的整数列表。
+ * 首先，从左到右，从第一个数字开始，每隔一个数字进行删除，直到列表的末尾。
+ * 第二步，在剩下的数字中，从右到左，从倒数第一个数字开始，每隔一个数字进行删除，直到列表开头。
+ * 我们不断重复这两步，从左到右和从右到左交替进行，直到只剩下一个数字。
+ * 返回长度为 n 的列表中，最后剩下的数字。
+ * 
+ * 示例：
+ * 
+ * 
+ * 输入:
+ * n = 9,
+ * 1 2 3 4 5 6 7 8 9
+ * 2 4 6 8
+ * 2 6
+ * 6
+ * 
+ * 输出:
+ * 6
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int lastRemaining(int n) {
+        return n == 1 ? 1 : 2 * (1 + n / 2 - lastRemaining(n / 2));
+    }
+}
+// @lc code=end
+
