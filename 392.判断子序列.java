0ca08f4f04d4f063bc6ef5b961b5235460ad913f
tongluyang <tongluyang@gmail.com>
2020-06-02 13:18:59 +0800

    判断子序列

---
+++ 392.判断子序列.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=392 lang=java
+ *
+ * [392] 判断子序列
+ *
+ * https://leetcode-cn.com/problems/is-subsequence/description/
+ *
+ * algorithms
+ * Easy (48.93%)
+ * Likes:    180
+ * Dislikes: 0
+ * Total Accepted:    40.9K
+ * Total Submissions: 83.6K
+ * Testcase Example:  '"abc"\n"ahbgdc"'
+ *
+ * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+ * 
+ * 你可以认为 s 和 t 中仅包含英文小写字母。字符串 t 可能会很长（长度 ~= 500,000），而 s 是个短字符串（长度 <=100）。
+ * 
+ * 
+ * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+ * 
+ * 示例 1:
+ * s = "abc", t = "ahbgdc"
+ * 
+ * 返回 true.
+ * 
+ * 示例 2:
+ * s = "axc", t = "ahbgdc"
+ * 
+ * 返回 false.
+ * 
+ * 后续挑战 :
+ * 
+ * 如果有大量输入的 S，称作S1, S2, ... , Sk 其中 k >= 10亿，你需要依次检查它们是否为 T
+ * 的子序列。在这种情况下，你会怎样改变代码？
+ * 
+ * 致谢:
+ * 
+ * 特别感谢 @pbrother 添加此问题并且创建所有测试用例。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int start = 0;
+        for (char c : s.toCharArray()) {
+            int idx = t.indexOf(c, start);
+            if (idx < 0) {
+                return false;
+            }
+            start = idx + 1;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
