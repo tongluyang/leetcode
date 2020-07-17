170c1678b79e1be39de7d8fe12fa6308ea564ba7
tongluyang <tongluyang@gmail.com>
2020-07-17 13:06:34 +0800

    搜索插入位置 二分

--- 35.搜索插入位置.java
+++ 35.搜索插入位置.java
@@ -44,12 +44,19 @@
  */
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= target) {
-                return i;
+        int l = 0;
+        int r = nums.length;
+        while (l < r) {
+            int mid = (l + r) >>> 1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                r = mid;
+            } else {
+                l = mid + 1;
             }
         }
-        return nums.length;
+        return l;
     }
 }
 
